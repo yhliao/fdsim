856d053e6e15388afe81efd22ebc371c5fe41992
Henry Liao <henryl1152@gmail.com>
2016-05-30 14:41:42 +0800

    updates for NCFET simulation

--- PZTsim.py
+++ PZTsim.py
@@ -67,7 +67,7 @@ ws.write(0,1,'Vfe')
 ws.write(0,2,'Vpoly')
 ws.write(0,3,'Vtotal')
 row = 0
-for V in np.linspace(0.2,-4,60):
+for V in np.linspace(0.2,-4,70):
    row = row+1
    cl.V = V
    s.reset_EcBV()
@@ -78,6 +78,7 @@ for V in np.linspace(0.2,-4,60):
    Vfe   = -s.Ec[base] + s.Ec[-tSi-1]
    Vpoly = cr.Ec - s.Ec[-tSi]
    print ("Q= {} C/m^2".format(Q))
+   Vtot  = -s.Ec[base] + cr.Ec + 0.7
    Vtot  = Vfe + Vpoly
    print ("V= {} V".format(Vtot))
    print ("Vapp={}".format(V))
--- results/NCFETsim.py
+++ results/NCFETsim.py
@@ -21,18 +21,19 @@ picname   = "FIG/poly{}_FE{}nm_Dit{:.1E}.png"\
 
 def FEplusMOS(Qstack,Vstack,QMOS,VMOS,ID) :
    UNDEF = 100
-   Vstack = np.interp(QMOS,Qstack,Vstack,left=UNDEF, right=UNDEF)
+   Vstack = np.interp(QMOS*1,Qstack,Vstack,left=UNDEF, right=UNDEF)
    valid_idx = (Vstack!=UNDEF)
    QMOS   = QMOS[valid_idx]
    VNCFET = VMOS[valid_idx] + Vstack[valid_idx]
    ID     = ID[valid_idx]
-   return QMOS, VNCFET, ID
+   return QMOS, VNCFET, ID, Vstack[valid_idx]
 
-def show():
+def show(show=True,save=True):
    fig = plt.figure(figsize=(20,10))
    ### plot the Q-V relations
    plt.subplot(1,3,1)
    plt.plot(V_NCFET,Q_MOS, label='NCFET')
+   plt.plot(vv,Q_MOS, label='stack')
    plt.plot(VMOS,   QMOS,  label='MOS')
    plt.legend(loc='upper right')
    plt.title('Q-V relations')
@@ -55,8 +56,10 @@ def show():
    plt.title('I-V relations')
    plt.xlabel('VG')
    plt.ylabel('ID')
-   #plt.show()
-   fig.savefig(picname)
+   if show:
+      plt.show()
+   if save:
+      fig.savefig(picname)
 
 def calcSS(VG,ID):
    i1 = np.min(np.where(ID>1E-5))
@@ -81,14 +84,14 @@ except:
 wb = xlrd.open_workbook('MOS_IQV.xls')
 sMOS = wb.sheet_by_name('mos')
 
-Qstack = -np.array(sFE.col_values(0,start_rowx=1))
+Qstack = -np.array(sFE.col_values(0,start_rowx=1)) 
 Vstack = -np.array(sFE.col_values(3,start_rowx=1))
 assert len(Qstack) == len(Vstack)
-QMOS   = np.array(sMOS.col_values(1,start_rowx=1))
+QMOS   = np.array(sMOS.col_values(1,start_rowx=1)) 
 VMOS   = np.array(sMOS.col_values(0,start_rowx=1))
 ID     = np.array(sMOS.col_values(2,start_rowx=1))
 assert len(QMOS)== len(VMOS) == len(ID)
-Q_MOS, V_NCFET, I_D = FEplusMOS(Qstack,Vstack,QMOS,VMOS,ID)
+Q_MOS, V_NCFET, I_D,vv = FEplusMOS(Qstack,Vstack,QMOS,VMOS,ID)
 calcSS(VMOS,ID)
 calcSS(V_NCFET,I_D)
-show()
+show(show=False)
