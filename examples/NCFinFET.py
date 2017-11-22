4b424cf6519e0e4cb564e5d25b5c98b520e03f8c
jpduarte <jpduarte@berkeley.edu>
2017-11-22 02:33:10 -0800

    run several data for NCFET and FinFET

--- examples/NCFinFET.py
+++ examples/NCFinFET.py
@@ -2,134 +2,131 @@
 import sys
 sys.path.append("../")
 from solver.dev_sim import dev_solver2D
-from solver.const import mdb
+from solver.const import mdb, kBT
 import numpy as np
 import csv
 import pickle
 import pylab
 
 ################################################################################
+NBODY_array = {1e24,5e24}
+for NBODY in NBODY_array:
+    device='ncfet'
+    NSD =1e20*1e6
+    Lg  = 25e-9
+    tinsf = 1.2e-9
+    tinsb = tinsf
+    Tch   = 8e-9
+    Ls = 20e-9
+    Ld = 20e-9
+    dx = 0.2e-9
+    dy = 1e-9
+    Vref=0.0
+    PHIG=4.5
+    #NBODY=1e21
+    Vds = 0.5
+    alpha = -3e9 #;%m/F alpha=-1.61379679e-2
+    beta = 6.5e11 #;%6e11;%C^2m^5/F 6.52372352e-5*1e12
+    tfe = 3e-9#;%m
+    Vref = 1.0
+    vgpoints=20
 
-NSD =1e20*1e6
-Lg  = 25e-9
-tinsf = 2e-9
-tinsb = 2e-9
-Tch   = 10e-9
-Ls = 20e-9
-Ld = 20e-9
-dx = 0.5e-9
-dy = 1e-9
-alpha = -3e9 #;%m/F alpha=-1.61379679e-2
-beta = 6.5e11 #;%6e11;%C^2m^5/F 6.52372352e-5*1e12
-tfe = 3e-9#;%m
-Vref = 1.0
-################################################################################
+    ################################################################################
+    def VFE(qfe,alpha,beta,tfe):
+       return 2*alpha*tfe*qfe+4*beta*tfe*qfe**3
 
-s = dev_solver2D(dx,dy)
-#m0 = s.add_mesh(N=[10,100] , pos=[-10,0], material='Si') #gate
-m1 = s.add_mesh(N=[int(np.round((tinsf/dx))),int(np.round((Lg/dy)))] , pos=[0,0] ,material='SiO2')
-m2 = s.add_mesh(N=[int(np.round((Tch/dx))),int(np.round(((Lg+Ls+Ld)/dy)))], pos=[int(np.round((tinsf/dx))),int(-np.round((Ls/dy)))],material='Si')
-m3 = s.add_mesh(N=[int(np.round((tinsb/dx))),int(np.round((Lg/dy)))] , pos=[int(np.round(((tinsf+Tch)/dx))),0] ,material='SiO2')
-#m0.NB[:] = 1E19 * 1E6
-m2.NB[:] = -1e16 * 1e6
-m2.NB[:, 0:int(np.round(Ls/dy))] = NSD
-m2.NB[:, int(np.round(((Lg+Ld)/dy))):] = NSD
+    def find_nearest(a, a0):
+        "Element in nd array `a` closest to the scalar value `a0`"
+        idx = np.abs(a - a0).argmin()
+        return a.flat[idx],idx
 
-cg = s.add_contact(-1,[0,int(np.round(Lg/dy))]) #(x=-11), y=0 to 100
-cs = s.add_contact([int(np.round(tinsf/dx)),int(np.round(((Tch+tinsf)/dx)))],int(-np.round(Ls/dy+1)))
-cd = s.add_contact([int(np.round(tinsf/dx)),int(np.round(((Tch+tinsf)/dx)))],int(np.round(((Lg+Ld)/dy))))
-cgb = s.add_contact(int(np.round((Tch+tinsf+tinsb)/dx)),[0, int(np.round((Lg/dy)))])
+    s = dev_solver2D(dx,dy)
+    #m0 = s.add_mesh(N=[10,100] , pos=[-10,0], material='Si') #gate
+    m1 = s.add_mesh(N=[int(np.round((tinsf/dx))),int(np.round((Lg/dy)))] , pos=[0,0] ,material='SiO2')
+    m2 = s.add_mesh(N=[int(np.round((Tch/dx))),int(np.round(((Lg+Ls+Ld)/dy)))], pos=[int(np.round((tinsf/dx))),int(-np.round((Ls/dy)))],material='Si')
+    m3 = s.add_mesh(N=[int(np.round((tinsb/dx))),int(np.round((Lg/dy)))] , pos=[int(np.round(((tinsf+Tch)/dx))),0] ,material='SiO2')
+    #m0.NB[:] = 1E19 * 1E6
+    m2.NB[:] = -NBODY
+    m2.NB[:, 0:int(np.round(Ls/dy))] = NSD
+    m2.NB[:, int(np.round(((Lg+Ld)/dy))):] = NSD
 
-
-def VFE(qfe,alpha,beta,tfe):
-   return 2*alpha*tfe*qfe+4*beta*tfe*qfe**3
-
-def find_nearest(a, a0):
-    "Element in nd array `a` closest to the scalar value `a0`"
-    idx = np.abs(a - a0).argmin()
-    return a.flat[idx],idx
-
-ni = mdb['Si'].ni
-cs.n = NSD
-cs.p = ni**2/NSD
-cd.n = NSD
-cd.p = ni**2/NSD
-
-cg.n = 1e19 * 1e6
-cg.p = 1e1 * 1e6
-cgb.n = 1e19 * 1e6
-cgb.p = 1e1 * 1e6
-
-#s.visualize(['Ec','Ev'])
-s.construct_profile()
-
-### The B.C can be either vector or number
-cs.V = Vref
-cd.V = Vref+0.05
-
-f = open("SOIinfo-T.csv",'wb')
-writer = csv.writer(f)
-step = 20
-Vg = np.linspace(0,1,step)+Vref
-
-IDn   = np.empty(step)
-IDp   = np.empty(step)
-IDn_t = np.empty(step)
-IDp_t = np.empty(step)
-
-Ign   = np.empty(step)
-Igp   = np.empty(step)
-Ign_t = np.empty(step)
-Igp_t = np.empty(step)
-
-prefix='NC1_0005V_25nm_'
-Vg_file = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn_file = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg_file = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+    cg = s.add_contact(-1,[0,int(np.round(Lg/dy))]) #(x=-11), y=0 to 100
+    cs = s.add_contact([int(np.round(tinsf/dx)),int(np.round(((Tch+tinsf)/dx)))],int(-np.round(Ls/dy+1)))
+    cd = s.add_contact([int(np.round(tinsf/dx)),int(np.round(((Tch+tinsf)/dx)))],int(np.round(((Lg+Ld)/dy))))
+    cgb = s.add_contact(int(np.round((Tch+tinsf+tinsb)/dx)),[0, int(np.round((Lg/dy)))])
 
 
-prefix='0005V_25nm_'
-Vg_file2 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn_file2 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg_file2 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+    ni = mdb['Si'].ni
+    cs.n = NSD
+    cs.p = ni**2/NSD
+    cd.n = NSD
+    cd.p = ni**2/NSD
+
+    cg.n = 1e19 * 1e6
+    cg.p = 1e1 * 1e6
+    cgb.n = 1e19 * 1e6
+    cgb.p = 1e1 * 1e6
+
+    #s.visualize(['Ec','Ev'])
+    s.construct_profile()
+
+    ### The B.C can be either vector or number
+    cs.V = Vref
+    cd.V = Vref+Vds
+
+    f = open("SOIinfo-T.csv",'wb')
+    writer = csv.writer(f)
+    Vg = np.linspace(0,1,vgpoints)
+    Vgeff = Vg+Vref-(PHIG-(mdb['Si'].phiS+mdb['Si'].Eg/2+ kBT*np.log(NBODY/ni)))-kBT*np.log(NBODY*NSD/ni**2)
 
 
-Qg_array  = []
+    IDn   = np.empty(vgpoints)
+    IDp   = np.empty(vgpoints)
+    IDn_t = np.empty(vgpoints)
+    IDp_t = np.empty(vgpoints)
 
-for n,V in enumerate(Vg):
-   valueV,indexvg = find_nearest(Vg,V)
-   cg.V= V*np.ones(int(np.round((Lg/dy))))-VFE(Qg_file[:,indexvg],alpha,beta,tfe)
-   cgb.V= V*np.ones(int(np.round((Lg/dy))))-VFE(Qg_file[:,indexvg],alpha,beta,tfe)
+    Ign   = np.empty(vgpoints)
+    Igp   = np.empty(vgpoints)
+    Ign_t = np.empty(vgpoints)
+    Igp_t = np.empty(vgpoints)
 
-   #filename1 = "FDSOI_Vg{}.dat".format(V)
-   #output    = open(filename1,'wb')
-   s.solve(1e-3,False,False)
-   (IDn[n],IDp[n]) = (cd.In, cd.Ip)
-   (Ign[n],Igp[n]) = (cg.In, cg.Ip)
-   print ("**** VG={}, IDn={}, Ig={} ***".format(V,-cd.In,cg.In))
-   Qg_array.append(cg.D)
-   #s.visualize(['Ec','Ev','Efn','Efp'])
-   #m2.cshow('n')
-   #pickle.dump(s,output)
+    Qg_array  = []
+    Vfe_array = []
 
-pylab.figure(1)
-pylab.plot(Vg, IDn,'o')
-pylab.plot(Vg, IDn_file,'s')
-pylab.plot(Vg, IDn_file2,'>')
+    vfe_voltage = np.zeros(int(np.round((Lg/dy))))
+    for n,V in enumerate(Vgeff):
+       for i in range(3):
+           cg.V = V*np.ones(int(np.round((Lg/dy))))-vfe_voltage
+           cgb.V = cg.V
+           s.solve(1e-3,False,False)
+           vfe_voltage_old = vfe_voltage
+           vfe_voltage = VFE(cg.D,alpha,beta,tfe)
+           print ('VFE sum diff:')
+           print (np.sum(abs(vfe_voltage_old-vfe_voltage)))
 
+       (IDn[n],IDp[n]) = (cd.In, cd.Ip)
+       (Ign[n],Igp[n]) = (cg.In, cg.Ip)
+       print ("**** VG={}, IDn={}, Ig={} ***".format(V,-cd.In,cg.In))
+       Qg_array.append(cg.D)
+       Vfe_array.append(vfe_voltage)
+       #s.visualize(['Ec','Ev','Efn','Efp'])
+       #m2.cshow('n')
+       #pickle.dump(s,output)
 
-pylab.figure(2)
-pylab.plot(Vg, IDn,'o')
-pylab.plot(Vg, IDn_file,'s')
-pylab.plot(Vg, IDn_file2,'>')
-pylab.yscale('log')
+    pylab.figure(1)
+    pylab.plot(Vg, IDn,'o')
 
-s.visualize(['Ec','Ev','Efn','Efp'])
+    pylab.figure(2)
+    pylab.plot(Vg, IDn,'o')
+    pylab.yscale('log')
 
-### Simple access for the displacements at the contact
-print (cs.D)
-prefix='NC2_0005V_25nm_'
-np.savetxt(prefix+'Vg_data.txt', Vg, delimiter=',')
-np.savetxt(prefix+'IDn_data.txt', IDn, delimiter=',')
-np.savetxt(prefix+'Qg_data_new.txt', np.transpose(Qg_array), delimiter=',')
+    #s.visualize(['Ec','Ev','Efn','Efp'])
+
+    ### Simple access for the displacements at the contact
+    #save vfe, potential front, potentia
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_NBODY'+str(NBODY)+'_VDS'+str(Vds)
+    np.savetxt(prefix+'_Vg_data.txt', Vg, delimiter=',')
+    np.savetxt(prefix+'_IDn_data.txt', IDn, delimiter=',')
+    np.savetxt(prefix+'_Qg_data_new.txt', np.transpose(Qg_array), delimiter=',')
+    np.savetxt(prefix+'_Vfe_data_new.txt', np.transpose(Vfe_array), delimiter=',')
 pylab.show()
--- examples/data_read.py
+++ examples/data_read.py
@@ -3,19 +3,25 @@ import csv
 import pickle
 import pylab
 
+device='ncfet'
 NSD =1e20*1e6
 Lg  = 25e-9
-tinsf = 1e-9
-tinsb = 1e-9
-Tch   = 10e-9
+tinsf = 1.2e-9
+tinsb = tinsf
+Tch   = 8e-9
 Ls = 20e-9
 Ld = 20e-9
-dx = 0.5e-9
+dx = 0.2e-9
 dy = 1e-9
-
+Vref=0.0
+PHIG=4.5
+NBODY=1e21
+Vds = 0.5
 alpha = -3e9 #;%m/F alpha=-1.61379679e-2
-beta =6.5e11 #;%6e11;%C^2m^5/F 6.52372352e-5*1e12
-tfe=3e-9#;%m
+beta = 6.5e11 #;%6e11;%C^2m^5/F 6.52372352e-5*1e12
+tfe = 0#;%m
+Vref = 1.0
+vgpoints=20
 
 def VFE(qfe,alpha,beta,tfe):
    return 2*alpha*tfe*qfe+4*beta*tfe*qfe**3
@@ -25,99 +31,207 @@ def find_nearest(a, a0):
     idx = np.abs(a - a0).argmin()
     return a.flat[idx],idx
 
-prefix='0005V_25nm_'
-Vg0 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn0 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg0 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+tfe_array = {0,1e-9,2e-9,3e-9}#;%m
 
-prefix='05V_25nm_'
-Vg = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+for tfe in tfe_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
 
-prefix='NC_05V_25nm_'
-Vg2 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn2 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg2 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+    pylab.figure(1)
+    pylab.plot(Vg, IDn,'-')
+
+    pylab.figure(2)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
+
+tfe = 0#;%m
+Lg_array = {20e-9,25e-9,30e-9}#;%m
+for Lg in Lg_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(3)
+    pylab.plot(Vg, IDn,'-')
+
+    pylab.figure(4)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
+
+tfe = 3e-9#;%m
+Lg_array = {20e-9,25e-9,30e-9}#;%m
+for Lg in Lg_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(3)
+    pylab.plot(Vg, IDn,'--')
+
+    pylab.figure(4)
+    pylab.plot(Vg, IDn,'--')
+    pylab.yscale('log')
+
+tfe = 0#;%m
+Lg=25e-9
+Tch_array = {6e-9,8e-9,10e-9}#;%m
+for Tch in Tch_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(5)
+    pylab.plot(Vg, IDn,'-')
+
+    pylab.figure(6)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
+
+tfe = 3e-9#;%m
+Lg=25e-9
+Tch_array = {6e-9,8e-9,10e-9}#;%m
+for Tch in Tch_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(5)
+    pylab.plot(Vg, IDn,'--')
+
+    pylab.figure(6)
+    pylab.plot(Vg, IDn,'--')
+    pylab.yscale('log')
 
 
-prefix='NC2_05V_25nm_'
-Vg3 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn3 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg3 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+tfe = 0#;%m
+Lg=25e-9
+Tch=8e-9
+tins_array = {0.8e-9,1.2e-9,1.6e-9}#;%m
+for tinsf in tins_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(7)
+    pylab.plot(Vg, IDn,'-')
+
+    pylab.figure(8)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
+
+tfe = 3e-9#;%m
+Lg=25e-9
+Tch=8e-9
+tins_array = {0.8e-9,1.2e-9,1.6e-9}#;%m
+for tinsf in tins_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(7)
+    pylab.plot(Vg, IDn,'--')
+
+    pylab.figure(8)
+    pylab.plot(Vg, IDn,'--')
+    pylab.yscale('log')
 
 
-prefix='NC3_05V_25nm_'
-Vg4 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn4 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg4 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+tfe = 0#;%m
+Lg=25e-9
+Tch=8e-9
+tinsf=1.2e-9
+NBODY_array = {1e21}
+for NBODY in NBODY_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(9)
+    pylab.plot(Vg, IDn,'-')
+
+    pylab.figure(10)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
 
 
-prefix='NC1_0005V_25nm_'
-Vg5 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn5 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg5 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+NBODY_array = {1e24,5e24}#;%m+'_NBODY'+str(NBODY)
+for NBODY in NBODY_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_NBODY'+str(NBODY)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
 
-prefix='NC2_0005V_25nm_'
-Vg6 = np.loadtxt(prefix+'Vg_data.txt', delimiter=',')
-IDn6 = np.loadtxt(prefix+'IDn_data.txt', delimiter=',')
-Qg6 = np.loadtxt(prefix+'Qg_data_new.txt', delimiter=',')
+    pylab.figure(9)
+    pylab.plot(Vg, IDn,'-')
 
-pylab.figure(1)
-pylab.plot(Vg, IDn0,'-')
-pylab.plot(Vg, IDn,'o')
-pylab.plot(Vg, IDn2, 's')
-pylab.plot(Vg, IDn3, '<')
-pylab.plot(Vg, IDn4, '*')
-pylab.plot(Vg, IDn5, '-')
-pylab.plot(Vg, IDn6, '-')
-pylab.figure(2)
-pylab.plot(Vg, IDn0,'-')
-pylab.plot(Vg, IDn,'o')
-pylab.plot(Vg, IDn2, 's')
-pylab.plot(Vg, IDn3, '<')
-pylab.plot(Vg, IDn4, '*')
-pylab.plot(Vg, IDn5, '-')
-pylab.plot(Vg, IDn6, '-')
-pylab.yscale('log')
+    pylab.figure(10)
+    pylab.plot(Vg, IDn,'-')
+    pylab.yscale('log')
+
+tfe = 3e-9#;%m
+NBODY_array = {1e21}
+for NBODY in NBODY_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
+
+    pylab.figure(9)
+    pylab.plot(Vg, IDn,'--')
+
+    pylab.figure(10)
+    pylab.plot(Vg, IDn,'--')
+    pylab.yscale('log')
 
 
-SSfinfet = 1e3*np.diff(Vg)/ np.diff(np.log10(IDn))
-SSncfinfet = 1e3*np.diff(Vg)/ np.diff(np.log10(IDn4))
+NBODY_array = {1e24,5e24}#;%m+'_NBODY'+str(NBODY)
+for NBODY in NBODY_array:
+    prefix='../results_juan/'+device+'_Lg'+str(Lg)+'_tins'+str(tinsf)+'_Tch'+str(Tch)+'_tfe'+str(tfe)+'_NBODY'+str(NBODY)+'_VDS'+str(Vds)
+    Vg = np.loadtxt(prefix+'_Vg_data.txt', delimiter=',')
+    IDn = np.loadtxt(prefix+'_IDn_data.txt', delimiter=',')
+    Qg = np.loadtxt(prefix+'_Qg_data_new.txt', delimiter=',')
+    Vfe = np.loadtxt(prefix+'_Vfe_data_new.txt', delimiter=',')
 
-pylab.figure(3)
-pylab.plot(Vg[:-1], SSfinfet,'o-')
-pylab.plot(Vg[:-1], SSncfinfet,'s-')
+    pylab.figure(9)
+    pylab.plot(Vg, IDn,'--')
 
-SSfinfet = 1e3*np.diff(Vg)/ np.diff(np.log10(IDn0))
-SSncfinfet = 1e3*np.diff(Vg)/ np.diff(np.log10(IDn6))
-
-pylab.figure(4)
-pylab.plot(Vg[:-1], SSfinfet,'o-')
-pylab.plot(Vg[:-1], SSncfinfet,'s-')
+    pylab.figure(10)
+    pylab.plot(Vg, IDn,'--')
+    pylab.yscale('log')
 
 '''
+SSfinfet = 1e3*np.diff(Vg0)/ np.diff(np.log10(IDn0))
+SSncfinfet = 1e3*np.diff(Vg0)/ np.diff(np.log10(IDn1))
 
 pylab.figure(3)
-pylab.plot(np.linspace(0,1,int(np.round((Lg/dy)))), Qg)
+pylab.plot(Vg0[:-1], SSfinfet,'o-')
+pylab.plot(Vg0[:-1], SSncfinfet,'s-')
+
+SSfinfet = 1e3*np.diff(Vg0)/ np.diff(np.log10(IDn0))
+SSncfinfet = 1e3*np.diff(Vg0)/ np.diff(np.log10(IDn1))
+
 pylab.figure(4)
-pylab.plot(np.linspace(0,1,int(np.round((Lg/dy)))), VFE(Qg,alpha,beta,tfe))
-
-
-test = np.loadtxt('test_data.txt',  delimiter=',')
-
-print (test[:,0])
-
-arr = []
-
-arr.append(Vg)
-arr.append(IDn)
-np.savetxt('test_data.txt',  np.transpose(arr), delimiter=',')
+pylab.plot(Vg0[:-1], SSfinfet,'o-')
+pylab.plot(Vg0[:-1], SSncfinfet,'s-')
 '''
-value,indexvg = find_nearest(Vg,0.3)
-print(indexvg)
-print(Qg[:,6])
-
-
 
 pylab.show()
